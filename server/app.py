c212e5be6a07a05b3db7785762969eacab6acc3f
manoj sagar <manojsagar066@gmail.com>
2021-07-14 10:56:53 +0530

    connected to db

--- server/app.py
+++ server/app.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import pymongo
+from pymongo import MongoClient
+import bson
+from bson.json_util import dumps
+
+
+client = MongoClient("mongodb+srv://RFIDpayments:Ff6RfZyRN5arkgvz@payments-ukurt.mongodb.net/test?retryWrites=true&w=majority")
+db = client["voice_bill"]
+collection = db["items"]
+
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello Machas I am commiting"
+@app.route('/',methods=["GET"])
+def get_items():
+    items = collection.find({})
+    return dumps(items)
 
 if __name__ == "__main__":
     app.run(debug=True)
