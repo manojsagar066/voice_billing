ee9b7fda80d5922f120a6fd2121c9d995a51c429
Prajwal Ponnana <prajwalshiv.04@gmail.com>
2021-07-17 15:55:58 +0530

    Added drawer navigation and delete bill item logic

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -2,77 +2,108 @@ import {  createAppContainer } from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import { Icon } from 'react-native-elements';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity,Alert } from 'react-native';
 import React from 'react';
 import LoginScreen from '../screens/LoginScreen';
 import NewBillScreen from '../screens/NewBillScreen';
 import PreviousBillsScreen from '../screens/PreviousBillsScreen';
 import AddNewItemScreen from '../screens/AddNewItemScreen';
-const AddNewNavigator = createStackNavigator({
-    AddNewItemScreen:{
-        screen:AddNewItemScreen,
-        navigationOptions:{
-            headerTitle:'Add new item'
-        }
-    }
-},{
-    defaultNavigationOptions:{
-        headerStyle:{
-         backgroundColor:'#d6d3cb',
-        },
-        headerRight:()=>{
-            return (<TouchableOpacity>
-                <Icon style={{marginRight:10}} name='logout' type='material' size={30} color='#808e9b' />
-            </TouchableOpacity>)
-        }
-      }
-})
-const AppNavigator = createStackNavigator({
-
-    // LoginScreen : {
-    //     screen:LoginScreen,
-    //     navigationOptions:{
-    //         headerShown:false,
-    //          headerRight:()=>null
-    //     }
-    // },
-    // PreviousBillsScreen:{
-    //     screen:PreviousBillsScreen,
-    //     navigationOptions:{
-    //         headerTitle: "Bills generated so far",
-    //         headerLeft:()=>null
-    //     }
-    // },
-    NewBillScreen : {
-        screen:NewBillScreen,
-    },
-    
-},{
-    mode:'modal',
-    defaultNavigationOptions:{
-        headerStyle:{
-         backgroundColor:'#d6d3cb',
-        },
-        headerRight:()=>{
-            return (<TouchableOpacity>
-                <Icon style={{marginRight:10}} name='logout' type='material' size={30} color='#808e9b' />
-            </TouchableOpacity>)
-        }
-      }
-  });
-  const MainNavigator = createDrawerNavigator({
-      BillsPart:{
-          screen:AppNavigator,
-          navigationOptions:{
-              drawerLabel:"Billing"
-          }
+const AddNewNavigator = createStackNavigator(
+  {
+    AddNewItemScreen: {
+      screen: AddNewItemScreen,
+      navigationOptions: {
+        headerTitle: "Add new item",
+        headerLeft: () => null,
       },
-      AddNewPart:{
-          screen:AddNewNavigator,
-          navigationOptions:{
-            drawerLabel:"New Item",
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#d6d3cb",
+      },
+      headerRight: () => {
+        return (
+          <TouchableOpacity>
+            <Icon
+              style={{ marginRight: 10 }}
+              name="logout"
+              type="material"
+              size={30}
+              color="#808e9b"
+            />
+          </TouchableOpacity>
+        );
+      },
+    },
+  }
+);
+const AppNavigator = createStackNavigator(
+  {
+    PreviousBillsScreen: {
+      screen: PreviousBillsScreen,
+      navigationOptions: {
+        headerTitle: "Bills generated so far",
+        headerLeft: () => null,
+      },
+    },
+    NewBillScreen: {
+      screen: NewBillScreen,
+    },
+  },
+  {
+    mode: "modal",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#d6d3cb",
+      },
+    },
+  }
+);
+const MainNavigator = createDrawerNavigator(
+  {
+    BillsPart: {
+      screen: AppNavigator,
+      navigationOptions: {
+        drawerLabel: "Billing",
+      },
+    },
+    AddNewPart: {
+      screen: AddNewNavigator,
+      navigationOptions: {
+        drawerLabel: "New Item",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#d6d3cb",
+    drawerType: "back",
+    hideStatusBar: true,
+    keyboardDismissMode: "on-drag",
+    contentOptions: {
+      itemsContainerStyle: {
+        marginHorizontal: 5,
+      },
+    },
+  }
+);
+const AuthNavigator = createStackNavigator({
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: {
+      headerShown: false,
+      headerRight: () => null,
+    },
+  },
+  MainNavigator: {
+    screen: MainNavigator,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
 
-        }
-      }
-  })
-export default createAppContainer(AppNavigator);
+
+  
+export default createAppContainer(AuthNavigator);
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,10 @@
-import React, { useEffect,useState } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
+import React, {useState } from 'react';
+import {useDispatch } from 'react-redux';
 import { login } from '../store/actions/appActions';
 import { androidClientId ,facebookAppId} from "../helpers/authIds";
 import {
   StyleSheet,
-  Text,
+  Text,ScrollView,
   View,
   ActivityIndicator,
   TextInput,
@@ -12,7 +12,6 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity,
 } from 'react-native';
-
 import { Icon } from 'react-native-elements';
 import * as Google from 'expo-google-app-auth';
 import * as Facebook from 'expo-facebook';
@@ -26,10 +25,9 @@ export default function LoginScreen(props) {
   const [isRegistered,updateIsRegistered] = useState(true);
   const [isLoadingGoogle,setIsLoadingGoogle] = useState(false);
   const [isLoadingFacebook,setIsLoadingFacebook] = useState(false);
-  const [error,setError] = useState("")
-  const [valid,setValid] = useState(true)
   const dispatchAction = useDispatch();
   const handleGoogleAuth = ()=>{
+    setIsLoadingGoogle(true);
     const config = {
       androidClientId : androidClientId,
       scopes:['profile','email']
@@ -37,11 +35,9 @@ export default function LoginScreen(props) {
     Google.logInAsync(config).then((res)=>{
       const {type,user} = res;
       if(type == 'success'){
-        console.log(user.name);
         dispatchAction(login({name:user.name,id:user.id}));
         props.navigation.navigate({
-          routeName: 'PreviousBillsScreen',
-          params: {}
+          routeName: "MainNavigator",
         });
       }else{
 
@@ -50,8 +46,7 @@ export default function LoginScreen(props) {
     }).catch((err)=>{
       console.log(err);
       setIsLoadingGoogle(false);
-    })
-
+    });
   }
 
   const handleFacebookAuth = async()=>{
@@ -72,11 +67,9 @@ export default function LoginScreen(props) {
         if (type === 'success') {
           const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
           const body = await response.json();
-          console.log(body,expirationDate,permissions,declinedPermissions);
           dispatchAction(login(body));
           props.navigation.navigate({
-            routeName: 'PreviousBillsScreen',
-            params: {}
+            routeName: 'MainNavigator',
           });
         } else {
           // type === 'cancel'
@@ -88,102 +81,161 @@ export default function LoginScreen(props) {
       }
     }
     
-  
-
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
+    <ScrollView
+      style={{
+        backgroundColor: "#d6d3cb",
       }}
     >
-      <View style={styles.container}>
-        <Text style={styles.welcomeText}>Hello There!</Text>
-        <View style={{backgroundColor:'#307fc9',padding:40,alignSelf:'center',borderRadius:80,width:160,height:160,marginTop:40}}>
-            <Icon size={80} type='fontisto' name='mic' color='white'/>
-        </View>
-        <Text style={styles.loginText}>{isRegistered?"Login":"Register"}</Text>
-        <TextInput
-          onChange={(data)=>{
-            updateEmail(data.target.value);
-          }}
-          value={email}
-          placeholder='Email Address'
-          placeholderTextColor='#1f1e1c'
-          style={styles.input}
-          autoCorrect={true}
-          autoCompleteType='email'
-          keyboardType='email-address'
-          textContentType='emailAddress'
-        />
-        <View style={{flexDirection:'row'}}>
-        <TextInput
-          onChange={(data)=>{
-            updatePassword(data.target.value);
-          }}
-          value={password}
-          placeholder='Password'
-          placeholderTextColor='#1f1e1c'
-          style={styles.input}
-          secureTextEntry={!isVisible}
-          textContentType='password'
-        />
-        <View style={{margin:5}}>
-            <TouchableOpacity onPress={()=>{updateIsVisible((prev)=>!prev)}}>
-                <Icon color={isVisible?'#307fc9':'white'} type='fontisto' name='eye'/>
-            </TouchableOpacity>
-        </View>
-        </View>
-        {
-            isRegistered ? <View/>:
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <View style={styles.container}>
+          <Text style={styles.welcomeText}>
+            {!isRegistered ? "Hello There!" : "Welcome back"}
+          </Text>
+          <View
+            style={{
+              backgroundColor: "#307fc9",
+              padding: 40,
+              alignSelf: "center",
+              borderRadius: 80,
+              width: 160,
+              height: 160,
+              marginTop: 40,
+            }}
+          >
+            <Icon size={80} type="fontisto" name="mic" color="white" />
+          </View>
+          <Text style={styles.loginText}>
+            {isRegistered ? "Login" : "Register"}
+          </Text>
+          <TextInput
+            onChange={(data) => {
+              updateEmail(data.target.value);
+            }}
+            value={email}
+            placeholder="Email Address"
+            placeholderTextColor="#1f1e1c"
+            style={styles.input}
+            autoCorrect={true}
+            autoCompleteType="email"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+          />
+          <View style={{ flexDirection: "row" }}>
             <TextInput
-            onChange={(data)=>{
+              onChange={(data) => {
+                updatePassword(data.target.value);
+              }}
+              value={password}
+              placeholder="Password"
+              placeholderTextColor="#1f1e1c"
+              style={styles.input}
+              secureTextEntry={!isVisible}
+              textContentType="password"
+            />
+            <View style={{ margin: 5 }}>
+              <TouchableOpacity
+                onPress={() => {
+                  updateIsVisible((prev) => !prev);
+                }}
+              >
+                <Icon
+                  color={isVisible ? "#307fc9" : "white"}
+                  type="fontisto"
+                  name="eye"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          {isRegistered ? (
+            <View />
+          ) : (
+            <TextInput
+              onChange={(data) => {
                 updateRePassword(data.target.value);
               }}
-          value={rePassword}
-          placeholder='Re Enter password'
-          placeholderTextColor='#1f1e1c'
-          style={styles.input}
-          secureTextEntry={!isVisible}
-          textContentType='password'
-        />
-        }
-        <TouchableOpacity>
-          <Text style={styles.fpText}>{isRegistered?"Forgot Password?":""}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{
-          
-          props.navigation.navigate({
-            routeName: 'PreviousBillsScreen',
-            params: {}
-          });
-        }} style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>{isRegistered?"Login":"Register"}</Text>
-        </TouchableOpacity>
-        <View style={styles.loginWithBar}>
-          {isLoadingGoogle ? <ActivityIndicator size="large" color='#307fc9' />: <TouchableOpacity style={styles.iconButton}onPress={()=>{
-            setIsLoadingGoogle(true);
-            handleGoogleAuth();}}>
-            <Icon name='google' type='font-awesome' size={30} color='#808e9b' />
-          </TouchableOpacity>}
-          {isLoadingFacebook ? <ActivityIndicator size="large" color='#307fc9' />:<TouchableOpacity style={styles.iconButton} onPress={()=>{setIsLoadingFacebook(true);handleFacebookAuth();}}>
-            <Icon
-              name='facebook-square'
-              type='font-awesome'
-              size={30}
-              color='#808e9b'
+              value={rePassword}
+              placeholder="Re Enter password"
+              placeholderTextColor="#1f1e1c"
+              style={styles.input}
+              secureTextEntry={!isVisible}
+              textContentType="password"
             />
-          </TouchableOpacity>}
-        </View>
-        <View style={styles.signUpTextView}>
-          <Text style={styles.signUpText}>{isRegistered ?"Don't have an account?":"Have an account??"}</Text>
-          <TouchableOpacity onPress={()=>updateIsRegistered((prev)=>!prev)}>
-            <Text style={[styles.signUpText, { color: '#307fc9' }]}>
-              {isRegistered ?' Sign Up':' Sign In'}
+          )}
+          <TouchableOpacity>
+            <Text style={styles.fpText}>
+              {isRegistered ? "Forgot Password?" : ""}
             </Text>
           </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              props.navigation.navigate({
+                routeName: "MainNavigator",
+                params: {},
+              });
+            }}
+            style={styles.loginButton}
+          >
+            <Text style={styles.loginButtonText}>
+              {isRegistered ? "Login" : "Register"}
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.loginWithBar}>
+            {isLoadingGoogle ? (
+              <ActivityIndicator size="large" color="#307fc9" />
+            ) : (
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={() => {
+                  handleGoogleAuth(dispatchAction,setIsLoadingGoogle,props.navigation);
+                }}
+              >
+                <Icon
+                  name="google"
+                  type="font-awesome"
+                  size={30}
+                  color="#808e9b"
+                />
+              </TouchableOpacity>
+            )}
+            {isLoadingFacebook ? (
+              <ActivityIndicator size="large" color="#307fc9" />
+            ) : (
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={() => {
+                  setIsLoadingFacebook(true);
+                  handleFacebookAuth();
+                }}
+              >
+                <Icon
+                  name="facebook-square"
+                  type="font-awesome"
+                  size={30}
+                  color="#808e9b"
+                />
+              </TouchableOpacity>
+            )}
+          </View>
+          <View style={styles.signUpTextView}>
+            <Text style={styles.signUpText}>
+              {isRegistered ? "Don't have an account?" : "Have an account??"}
+            </Text>
+            <TouchableOpacity
+              onPress={() => updateIsRegistered((prev) => !prev)}
+            >
+              <Text style={[styles.signUpText, { color: "#307fc9" }]}>
+                {isRegistered ? " Sign Up" : " Sign In"}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 }
 
@@ -192,11 +244,10 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 20,
-    backgroundColor:'#d6d3cb'
   },
   welcomeText: {
-    fontSize: 20,
-    fontWeight: '900',
+    fontSize: 25,
+    fontWeight: 'bold',
     color: '#1f1e1c',
     alignSelf: 'center',
   },
@@ -227,7 +278,7 @@ const styles = StyleSheet.create({
   loginButton: {
     backgroundColor: '#307fc9',
     paddingVertical: 12,
-    borderRadius: 6,
+    borderRadius: 7,
     marginTop: 20,
   },
   loginButtonText: {
@@ -244,9 +295,11 @@ const styles = StyleSheet.create({
   },
   iconButton: {
     backgroundColor: '#333',
-    padding: 14,
+    padding: 15,
     marginHorizontal: 10,
-    borderRadius: 100,
+    borderRadius: 30,
+    height:60,
+    width:60
   },
   signUpTextView: {
     marginTop: 40,
