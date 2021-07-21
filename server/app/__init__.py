293b974ba80acab663f6f0186d55afe9dd4effad
Manoj Sagar <55632893+manojsagar066@users.noreply.github.com>
2021-07-21 22:44:44 +0530

    Update __init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,12 +7,23 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://RFIDpayments:Ff6RfZyRN5arkgvz@payments-ukurt.mongodb.net/test?retryWrites=true&w=majority")
 db = client["voice_bill"]
 collection = db["system_users"]
+bills = db["billsDemo"]
 item_collection = db["items_description"]
 
 
 @app.route("/")
 def home():
     return "His is home macha"
+
+@app.route("/addbill",methods=['POST'])
+def add_bill():
+    inputs = request.get_json(force=True)
+    customer = inputs["customer"]
+    total = inputs["total"]
+    items = inputs["items"]
+    userId = inputs["userId"]
+    bills.insert_one({"customer":customer,"total":total,"items":items,"userId":userId})
+    return {"response":"bill added successfully"}
 
 
 @app.route("/fetch", methods=["POST"])
@@ -24,12 +35,18 @@ def fetch_user_data():
     data_list = list(data)
     data_json = dumps(data_list)
     if data_list == []:
-        collection.insert_one({"_id":id, "username":username,"bills":[]})
-        data = collection.find({"_id":id,"username":username})
-        data_list = list(data)
-        data_json = dumps(data_list)
-        return data_json
+        collection.insert_one({"_id":id, "username":username})
+        return {"_id":id, "username":username, "bills":[]}
+        #data = collection.find({"_id":id,"username":username})
+        #data_list = list(data)
+        #data_json = dumps(data_list)
+        #return data_json
+    
     else:
+        data = bills.find({"userId":id})
+        data_list = list(data)
+        data_list.append({"username":username})
+        data_json = dumps(data_list)
         return data_json
 
 
